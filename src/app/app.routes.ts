440a7e8d23895e13f13c42e59d9116513c5d5da6
Harsha <17harshavardhan@gmail.com>
2026-01-02 19:44:53 +0530

    intial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './homepage/homepage';
+import { Skincare } from './skincare/skincare';
+import { Babycare } from './babycare/babycare';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Homepage
+    },
+    {
+        path: 'skincare',
+        component: Skincare
+    },
+    {
+        path: 'babycare',
+        component: Babycare
+    }
+];
